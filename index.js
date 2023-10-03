4ad7e224837b0f4714b93e13c79f456b19069b06
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-10-03 18:19:32 +0530

    refactor: cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const authRoutes = require('./routes/AuthRoutes')
  * APP
  */
 
-var whitelist = ['http://localhost:5173']
+var whitelist = ['https://awaas-kalpana.vercel.app/']
 var corsOptions = {
     credentials: true,
     origin: function (origin, callback) {
